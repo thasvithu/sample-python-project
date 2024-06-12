0df234815557168aa705e1de27f1c26020c692d7
thasvithu <thasvithu7@gmail.com>
2024-06-12 08:58:44 +0530

    Resolved merge conflicts

---
+++ main.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Division by zero error"
+
+if __name__ == "__main__":
+    print("Sample Python Program")
+    
